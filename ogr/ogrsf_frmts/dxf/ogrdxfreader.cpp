c4340b6fdcb629b90c729076b01ee2e06b74e737
Alex Barreto <axbarreto@lavabit.com>
2021-06-07 09:19:16 -0400

    Merge branch 'master' of github.com:OSGeo/gdal
    * 'master' of github.com:OSGeo/gdal:
      gdal/doc/source/tutorials/osr_api_tut.rst - add a reference to "CRS27", "CRS83" or "CRS84" in the "CRS and axis order" section
      gdal/ogr/ogrspatialreference.cpp - SetWellKnownGeogCS: update the docs, reformat WKT strings.
      DXF: Fix crash on malformed file (fixes #3936)
      Internal libtiff: fix when writing a IFD with a single tile that is a sparse one, on big endian hosts (fixes failures in cog.py tests)

--- ogr/ogrsf_frmts/dxf/ogrdxfreader.cpp
+++ ogr/ogrsf_frmts/dxf/ogrdxfreader.cpp
@@ -228,12 +228,15 @@ int OGRDXFReader::ReadValueRaw( char *pszValueBuf, int nValueBufSize )
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 02a6804ddc (Merge branch 'master' of github.com:OSGeo/gdal)
 =======
 >>>>>>> 8535d12504 (Merge branch 'master' of github.com:OSGeo/gdal)
 =======
 >>>>>>> a18296e047 (Merge branch 'master' of github.com:OSGeo/gdal)
+=======
+>>>>>>> fd71f123ff (Merge branch 'master' of github.com:OSGeo/gdal)
         // If nothing was read, we have prematurely reached the end of the file
         if( iEOL == iSrcBufferOffset )
 <<<<<<< HEAD:ogr/ogrsf_frmts/dxf/ogrdxfreader.cpp
@@ -323,11 +326,24 @@ int OGRDXFReader::ReadValueRaw( char *pszValueBuf, int nValueBufSize )
 =======
 =======
         // If nothing was read, we have reached the end of the file
+=======
+        // If nothing was read, we have prematurely reached the end of the file
+>>>>>>> 9587f59887 (Merge branch 'master' of github.com:OSGeo/gdal)
         if( iEOL == iSrcBufferOffset )
+<<<<<<< HEAD:ogr/ogrsf_frmts/dxf/ogrdxfreader.cpp
             break;
 >>>>>>> 137c8e8f4e (Merge branch 'master' of github.com:OSGeo/gdal):gdal/ogr/ogrsf_frmts/dxf/ogrdxfreader.cpp
+<<<<<<< HEAD
 >>>>>>> a806a30646 (Merge branch 'master' of github.com:OSGeo/gdal)
+<<<<<<< HEAD
 >>>>>>> a18296e047 (Merge branch 'master' of github.com:OSGeo/gdal)
+=======
+=======
+=======
+            return -1;
+>>>>>>> 66a76c0d6d (Merge branch 'master' of github.com:OSGeo/gdal):gdal/ogr/ogrsf_frmts/dxf/ogrdxfreader.cpp
+>>>>>>> 9587f59887 (Merge branch 'master' of github.com:OSGeo/gdal)
+>>>>>>> fd71f123ff (Merge branch 'master' of github.com:OSGeo/gdal)
     }
 
     size_t nValueBufLen = 0;
