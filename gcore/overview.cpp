accab8c0ec703556367a2a36ba5f6c5ed24435cd
Alex Barreto <axbarreto@lavabit.com>
2021-06-16 15:33:25 -0400

    Merge branch 'master' of github.com:OSGeo/gdal
    * 'master' of github.com:OSGeo/gdal:
      makefile.vc: remove Unix commands in lib_dist target (#3986)
      QuadraticMeanFloatSSE2(): tiny perf improvement in SSE3 case
      Docker: update Alpine based images to Alpine 3.14 [ci skip]
      rasterio.py: add test for Byte averaging by a factor of 2
      VSIS3FSHandler::GetOptions(): fix non-XML conformity (fixes https://github.com/postgis/postgis/pull/618#discussion_r652143176)

--- gcore/overview.cpp
+++ gcore/overview.cpp
@@ -984,6 +984,7 @@ inline __m128 SQUARE(__m128 x)
     return _mm_mul_ps(x, x);
 }
 
+<<<<<<< HEAD:gcore/overview.cpp
 inline __m128 FIXUP_LANES(__m128 x)
 {
     return x;
@@ -1007,6 +1008,20 @@ inline __m128 FIXUP_LANES(__m128 x)
 #endif
 
 template<class T> static int NOINLINE QuadraticMeanFloatSSE2(int nDstXWidth,
+=======
+#ifdef __SSE3__
+#define sse2_hadd_ps _mm_hadd_ps
+#else
+inline __m128 sse2_hadd_ps(__m128 a, __m128 b)
+{
+    auto aEven_bEven = _mm_shuffle_ps(a, b, _MM_SHUFFLE(2,0,2,0));
+    auto aOdd_bOdd = _mm_shuffle_ps(a, b, _MM_SHUFFLE(3,1,3,1));
+    return _mm_add_ps(aEven_bEven, aOdd_bOdd); // (aEven + aOdd, bEven + bOdd)
+}
+#endif
+
+template<class T> static int QuadraticMeanFloatSSE2(int nDstXWidth,
+>>>>>>> 2e13b33fc5 (Merge branch 'master' of github.com:OSGeo/gdal):gdal/gcore/overview.cpp
                                                     int nChunkXSize,
                                                     const T*& CPL_RESTRICT pSrcScanlineShiftedInOut,
                                                     T* CPL_RESTRICT pDstScanline)
@@ -1127,9 +1142,7 @@ template<class T> static int NOINLINE QuadraticMeanFloatSSE2(int nDstXWidth,
         rms = FIXUP_LANES(rms);
 =======
         // Horizontal addition
-        const auto A = _mm_shuffle_ps(sumLo, sumHi, _MM_SHUFFLE(2,0,2,0));
-        const auto B = _mm_shuffle_ps(sumLo, sumHi, _MM_SHUFFLE(3,1,3,1));
-        const auto sumSquares = _mm_add_ps(A, B);
+        const auto sumSquares = sse2_hadd_ps(sumLo, sumHi);
 
         auto rms = _mm_mul_ps(maxV, _mm_sqrt_ps(_mm_mul_ps(sumSquares, zeroDot25)));
 
