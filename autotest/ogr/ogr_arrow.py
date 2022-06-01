44744601be070d30de45aa71a5a2198823797c7e
Even Rouault <even.rouault@spatialys.com>
2022-06-01 14:05:20 +0200

    Arrow: fix reading geometry columns whose type is registered as an Arrow extension (fixes #5834)

--- autotest/ogr/ogr_arrow.py
+++ autotest/ogr/ogr_arrow.py
@@ -392,6 +392,9 @@ def test_ogr_arrow_extension_type():
 
     gdal.Unlink(outfilename)
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c3927ff77e (Arrow: fix reading geometry columns whose type is registered as an Arrow extension (fixes #5834))
 
 
 ###############################################################################
@@ -425,5 +428,8 @@ def test_ogr_arrow_read_with_geoarrow_extension_registered():
         assert lyr.GetGeometryColumn() == 'geometry'
     finally:
         pa.unregister_extension_type(point_type.extension_name)
+<<<<<<< HEAD
 =======
 >>>>>>> 1c0e5e40b8 (Arrow: fix extension name key in field metadata (#5829))
+=======
+>>>>>>> c3927ff77e (Arrow: fix reading geometry columns whose type is registered as an Arrow extension (fixes #5834))
